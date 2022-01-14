3c5d5597763cd8c90627cc46034fbcea66924e1d
Edson Lima <edsoneduardo526@gmail.com>
2022-01-14 16:38:33 -0300

    Cria testes para o header

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function Index() {
     return (
         <div>
-            Inicio
+            <Header />
         </div>
     )
 }
---
+++ src/tests/header.test.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+beforeEach(() => {
+    render(<Header />);
+  });
+
+describe('Testa componente Header', () => {
+    test('O header deve possuir um titulo H1', () => {
+        expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+    });
+
+    test('O header deve possuir a logo da empresa', () => {
+        expect(screen.getByAltText('logo')).toBeInTheDocument();
+    });
+
+    test('O header deve possuir uma barra de navegação', () => {
+        expect(screen.getByRole('navigation')).toBeInTheDocument();
+        expect(screen.getByRole('listitem', { name: /inicio/i })).toBeInTheDocument();
+        expect(screen.getByRole('listitem', { name: /casas/i })).toBeInTheDocument();
+        expect(screen.getByRole('listitem', { name: /apartamentos/i })).toBeInTheDocument();
+    });
+
+    test('O header deve possuir a logo da empresa', () => {
+        expect(screen.getByTestId('filter-section')).toBeInTheDocument();
+    });
+
+    test('O header deve possuir as logos para das redes sociais', () => {
+        expect(screen.getAllByAltText('social-icon')).toHaveLength(3);
+    });
+});
